1abeb007fe66c381fad225ee3a16f53aaf6273ed
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2023-05-13 19:30:48 +0300

    feat: added ErrorPage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,11 +1,22 @@
 import React, { FC } from 'react';
 import style from './ErrorPage.module.css';
+import ButtonForm from '../../components/ui/ButtonForm/ButtonForm';
+import { Link } from 'react-router-dom';
 
 const ErrorPage: FC = () => {
   
   return (
-    <main className={style.wrapper}>
-      <h1>Что-то пошло не так</h1>
+    <main className={style.main}>
+      <h1 className={style.title}>404</h1>
+      <p className={style.subtitle}>
+        Кажется что-то пошло не так! Страница, которую вы запрашиваете, не
+        существует. Возможно она устарела, была удалена, или был введен неверный
+        адрес в адресной строке
+      </p>
+      <Link className={style.link} to='/'>
+        <ButtonForm text={'Перейти на главную'} />
+      </Link>
+      
     </main>
   );
 }
