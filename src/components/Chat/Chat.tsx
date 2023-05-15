b517111cc0d4fb375d79f609389ad18878ad98ae
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2023-05-15 14:41:15 +0300

    feat: component Contact & refactor component Chat

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,15 +1,23 @@
 import React, { FC } from 'react';
 import style from './Chat.module.css';
-import Avatar from '../ui/Avatar/Avatar';
-import IconButton from '../../components/ui/ButtonIcon/ButtonIcon';
 import avatar from '../../images/standart-avatar.png';
 import iconClose from '../../images/icons-close.svg'
 import HeaderPanel from '../ui/HeaderPanel/HeaderPanel';
+import ButtonIcon from '../ui/ButtonIcon/ButtonIcon';
+import iconSendMessage from '../../images/send-message.svg'
+import Input from '../ui/Input/Input';
 
 const Chat: FC = () => {
   return (
     <>
       <HeaderPanel avatar={avatar} icon={iconClose}  />
+      <div className={style.messages}>
+        <span>привет</span>
+      </div>
+      <form className={style.form}>
+        <Input type={'text'} placeholder={''} />
+        <ButtonIcon icon={iconSendMessage} />
+      </form>
     </>
   )
 }
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import style from './MainPage.module.css';
 import avatar from '../../images/standart-avatar.png';
 import iconMessage from '../../images/icons-message.svg';
-// import Preview from '../../components/Preview/Preview';
+import Preview from '../../components/Preview/Preview';
 import Header from '../../components/Header/Header';
 import Contact from '../../components/Contact/Contact';
 import Chat from '../../components/Chat/Chat';
